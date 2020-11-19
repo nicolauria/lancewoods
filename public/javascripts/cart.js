0a9b2ee523a0b117832d07e5d8a3bc8c3325059d
code-commit-at-711830799325 <code-commit-at-711830799325>
2020-11-19 15:36:16 -0800

    product id and quantity to sp controller add_to_cart on atc submit

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,3 +1,17 @@
-document.querySelector('#add-to-cart').onclick = function(e) {
-    e.preventDefault();
-}
+// document.querySelector('#add-to-cart').onclick = function(e) {
+//     e.preventDefault();
+
+//     const data = {
+//         productId: document.querySelector('#product-id').value,
+//         quantity: document.querySelector('#quantity').value
+//     }
+
+//     fetch('http://localhost:3000/add_to_cart', {
+//         method: 'post',
+//         body: JSON.stringify(data)
+//     }).then(function(response) {
+//         return response.json();
+//     }).then(function(data) {
+//         console.log(data);
+//     });
+// }
